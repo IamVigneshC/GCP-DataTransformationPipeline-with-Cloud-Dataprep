740b4a457a5d6b1acebb3a17536357f91ad62784
Vignesh Chandra Bose <67529089+IamVigneshC@users.noreply.github.com>
2020-08-30 21:50:10 -0700

    Create createtable.sql

---
+++ createtable.sql
@@ -0,0 +1,7 @@
+#standardSQL
+ CREATE OR REPLACE TABLE ecommerce.all_sessions_raw_dataprep
+ OPTIONS(
+   description="Raw data from analyst team to ingest into Cloud Dataprep"
+ ) AS
+ SELECT * FROM `data-to-insights.ecommerce.all_sessions_raw`
+ WHERE date = '20170801'; # limiting to one day of data 56k rows 
